6fb6f94ee258d74eda0aa8d3fb1f42263543991c
OmarAbdelrazek <omar.abdelrazek@outlook.com>
2022-10-21 20:07:15 +0200

    admin

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -1,4 +1,3 @@
-from enum import unique
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser
 from django.contrib.auth.models import PermissionsMixin
@@ -6,29 +5,27 @@ from django.contrib.auth.models import BaseUserManager
 
 
 class UserProfileManager(BaseUserManager):
-    """Manager for user profile"""
+    """Manager for user profiles"""
 
-    def create_user(self, name, email, password=None):
-        """Create new user"""
+    def create_user(self, email, name, password=None):
+        """Create a new user profile"""
         if not email:
-            raise ValueError("User must have an email")
+            raise ValueError('Users must have an email address')
 
         email = self.normalize_email(email)
-        user = self.model(name=name, email=email)
+        user = self.model(email=email, name=name,)
 
         user.set_password(password)
         user.save(using=self._db)
 
         return user
 
+    def create_superuser(self, email, name, password):
+        """Create and save a new superuser with given details"""
+        user = self.create_user(email, name, password)
 
-    def create_superuser(self, name, email, password):
-        """Create new superuser"""
-
-        user = self.create_user(name=name, email=email, password=password)
-        self.is_superuser = True
-        self.is_staff = True
-
+        user.is_superuser = True
+        user.is_staff = True
         user.save(using=self._db)
 
         return user
