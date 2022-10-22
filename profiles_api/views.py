3708ef981cf16792e4a1e06712fd568a22413aaa
OmarAbdelrazek <omar.abdelrazek@outlook.com>
2022-10-22 10:03:56 +0200

    added hello api

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+
+class HelloApiView(APIView):
+
+    def get(self, request, formate=None):
+        dum_list = [
+            "object1",
+            "object2",
+            "object3",
+            "object4",
+        ]
+        return Response({
+            "message": "Hello Api",
+            "dum_list": dum_list
+        })
