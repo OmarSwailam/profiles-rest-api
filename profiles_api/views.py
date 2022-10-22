916f52413c24513770f0823798537e7cd349b241
OmarAbdelrazek <omar.abdelrazek@outlook.com>
2022-10-22 11:56:47 +0200

    create post method

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,8 +1,11 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+from . import serializers
 
 
 class HelloApiView(APIView):
+    serializer_class = serializers.HelloSerializer
 
     def get(self, request, formate=None):
         dum_list = [
@@ -15,3 +18,16 @@ class HelloApiView(APIView):
             "message": "Hello Api",
             "dum_list": dum_list
         })
+    
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+        
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'Hello {name}!'
+            return Response({'message': message})
+        else:
+            return Response(
+                serializer.errors,
+                status=status.HTTP_400_BAD_REQUEST
+            )
